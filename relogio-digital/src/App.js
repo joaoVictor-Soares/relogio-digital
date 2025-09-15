ed8ec880e7cdfaf67b6a2b3a35e123a3fe1e7180
joaoVictor-Soares <jvtrgmlcmsoares@gmail.com>
2025-09-15 20:33:17 -0300

    NEW: criação de um relógio digital

---
+++ relogio-digital/src/App.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from "react";
+
+function Relogio(){
+  const [hora, setHora] = useState(new Date())
+
+  useEffect(() => {
+    // inicia um intervalo que atualiza a hora a cada segundo
+    const idIntervalo = setInterval(() => {
+      console.log("Atualizando o relógio...")
+      setHora(new Date())
+    }, 1000);
+
+    // retorna uma função de limpeza
+    return() => {
+      // limpa o intervalo para evitar vazamento de memória
+      clearInterval(idIntervalo);
+      console.log('Relógio parado. Intervalo limpo')
+    };
+  }, []);
+
+  return(
+    <div>
+      <h2>Relógio Digital</h2>
+      <p>A hora atual é: {hora.toLocaleDateString()}</p>
+    </div>
+  )
+}
+
+export default Relogio
